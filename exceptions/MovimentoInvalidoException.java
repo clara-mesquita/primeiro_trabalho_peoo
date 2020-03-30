b952733d3175566eeb3f822052b6ee64b3dc875a
clara-mesquita <62909135+clara-mesquita@users.noreply.github.com>
2020-03-30 18:30:44 -0300

    Add files via upload

---
+++ exceptions/MovimentoInvalidoException.java
@@ -0,0 +1,8 @@
+public class MovimentoInvalidoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    MovimentoInvalidoException(String message) {
+        super(message);
+    }
+}
