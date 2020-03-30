d31ffa9277bdd8fb8bc6329a2ded4cb0bdb74749
clara-mesquita <62909135+clara-mesquita@users.noreply.github.com>
2020-03-30 18:36:29 -0300

    Delete MovimentoInvalidoException.java

--- exceptions/MovimentoInvalidoException.java
+++
@@ -1,8 +0,0 @@
-public class MovimentoInvalidoException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-    MovimentoInvalidoException(String message) {
-        super(message);
-    }
-}
